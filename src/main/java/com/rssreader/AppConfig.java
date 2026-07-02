22e4b45d84a220e2769a896af0f1c61a486e04a6
不会游泳的鱼 <1451778027@qq.com>
2026-07-02 14:22:15 +0800

    自动更新优化

--- src/main/java/com/rssreader/AppConfig.java
+++ src/main/java/com/rssreader/AppConfig.java
@@ -12,7 +12,7 @@ public final class AppConfig {
     public static final String VERSION = "1.0.0";
 
     /** GitHub 仓库所有者 */
-    public static final String GITHUB_OWNER = "YOUR_USERNAME";
+    public static final String GITHUB_OWNER = "xiaoge1996122";
 
     /** GitHub 仓库名 */
     public static final String GITHUB_REPO = "rss-reader";
