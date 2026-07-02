694d496f964d05cac506014874825187f1fe74b3
不会游泳的鱼 <1451778027@qq.com>
2026-07-02 14:36:52 +0800

    release v1.0.3

--- src/main/java/com/rssreader/AppConfig.java
+++ src/main/java/com/rssreader/AppConfig.java
@@ -9,7 +9,7 @@ public final class AppConfig {
     private AppConfig() {}
 
     /** 当前版本号，与 git tag 保持一致 (不带 v 前缀) */
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     /** GitHub 仓库所有者 */
     public static final String GITHUB_OWNER = "xiaoge1996122";
